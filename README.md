712bc3692f658557de6fd5900f6aa34cbec27384
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-10-08 17:16:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## GZO Legend
+GZO Legend is an achievements extension for GZO Web.
+
+## Installation
+Download/clone and move the repository into the folder phpbb_root/ext/ganstaz/legend
+
+### Enable / Disable
+Go to admin panel / tab customise / Manage extensions -> enable / disable GZO Legend
+
+[![Build Status](https://github.com/GanstaZ/legend/workflows/Tests/badge.svg)](https://github.com/GanstaZ/legend)
---
+++ ext.php
@@ -0,0 +1,38 @@
+<?php
+/**
+*
+* GZO Web. An extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2022, GanstaZ, https://www.github.com/GanstaZ/
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace ganstaz\zodiac;
+
+/**
+* GZO Web: Legend
+*/
+class ext extends \phpbb\extension\base
+{
+	/**
+	* Enable Legend if GZO Web is enabled
+	*
+	* @return bool
+	* @access public
+	*/
+	public function is_enableable()
+	{
+		$is_enableable = true;
+
+		$ext_manager = $this->container->get('ext.manager');
+
+		if (!$ext_manager->is_enabled('ganstaz/web'))
+		{
+			$this->container->get('language')->add_lang('require', 'ganstaz/legend');
+			$is_enableable = false;
+		}
+
+		return $is_enableable;
+	}
+}
