b57d93fee143f4d8c71852f52868176474606d60
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-10-12 18:46:49 +0300

    add categories (types)

---
+++ core/type/anniversary.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* GZO Web. An extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2022, GanstaZ, https://www.github.com/GanstaZ/
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace ganstaz\legend\core\type;
+
+/**
+* GZO Web: Achievement type anniversary
+*/
+class anniversary extends base
+{
+}
