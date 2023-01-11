54b4c56c720d081e5c12ef1c6cdab7db1fddfce2
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2023-01-12 01:02:41 +0200

    update types

--- core/type/special.php
+++ core/type/special.php
@@ -13,6 +13,27 @@ namespace ganstaz\legend\core\type;
 /**
 * GZO Web: Achievement type special
 */
-class special extends base
+class special implements achievements_interface
 {
+	/**
+	* {@inheritdoc}
+	*/
+	public function get_legend_data(): array
+	{
+		return [
+			[
+				'user'   => 'user_id',
+				'points' => 'mixed',
+				'type'   => 'special',
+			]
+		];
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function set($data)
+	{
+		return [];
+	}
 }
