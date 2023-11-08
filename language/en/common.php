1c706a7b6e53ca43f89aae0e21c58b5f421c8bc1
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2023-11-08 20:32:00 +0200

    update

---
+++ language/en/common.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* An extension for the phpBB Forum Software package.
+*
+* @copyright (c) GanstaZ, https://www.github.com/GanstaZ/
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'GZO_LEGEND'       => 'Achievements',
+	'GZO_ACHIEVEMENTS' => 'Achievements',
+]);
--- migrations/m1_main.php
+++ migrations/m1_main.php
@@ -1,9 +1,9 @@
 <?php
 /**
 *
-* GZO Web. An extension for the phpBB Forum Software package.
+* An extension for the phpBB Forum Software package.
 *
-* @copyright (c) 2022, GanstaZ, https://www.github.com/GanstaZ/
+* @copyright (c) GanstaZ, https://www.github.com/GanstaZ/
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
@@ -36,7 +36,7 @@ class m1_main extends \phpbb\db\migration\migration
 	*/
 	static public function depends_on()
 	{
-		return array('\ganstaz\web\migrations\v24\m1_main');
+		return array('\ganstaz\gzo\migrations\v24\m1_main');
 	}
 
 	/**
